e7d8a92a30fb005ae971ec595c363de5003b4390
tud <tudual.quinio@protonmail.com>
2020-12-26 20:16:41 +0100

    format documents

--- src/adminCmds.js
+++ src/adminCmds.js
@@ -6,11 +6,11 @@ module.exports = {
     "wakeup": {
         description: "Use once to start and set up the bot.",
         process: (msg) => {
-            channel = msg.channel; // Channel where the message has been writeen and where we will respond
-            user = msg.member; // User that has send the message
-            if(data.setup === "ongoing") {
+            const channel = msg.channel; // Channel where the message has been writeen and where we will respond
+            const user = msg.member; // User that has send the message
+            if (data.setup === "ongoing") {
                 channel.send("There is already an admin going through the setup !");
-            } else if(data.setup) {
+            } else if (data.setup) {
                 channel.send("The bot has already been setup !")
             } else {
                 data.setup = "ongoing"; // Only one user can setup the bot
@@ -18,28 +18,32 @@ module.exports = {
                 channel.send("https://tenor.com/btkqj.gif");
                 channel.send("First of all, thank you for using tudbot! :heart:");
                 msg.reply("we need to set up few things before starting ! \nWhen you are ready to setup few things react with :white_check_mark: \nIf you want to use the default settings react with :fast_forward:").then(sentmsg => {
-                    
-                sentmsg.react('✅').then(sentmsg.react('⏩'));
-                const filter  = (reaction, user) => {
-                    return ['✅','⏩'].includes(reaction.emoji.name) && user.id === msg.author.id;
-                };
 
-                sentmsg.awaitReactions(filter,{max:1, time: 24*60*60*1000,error: ['time']}).then(collected => {
-                
-                const reaction = collected.first();
-                
-                if (reaction.emoji.name === '✅') {
-                let step = new Discord.MessageEmbed()
-                    .setColor(data.color)
-                    .setTitle('Step 1/?')
-                    .setDescription(':white_square_button: 1. Set the prefix. \n:white_square_button: 2. Set who has access to admins roles.');
-                
-                channel.send(step);
+                    sentmsg.react('✅').then(sentmsg.react('⏩'));
+                    const filter = (reaction, user) => {
+                        return ['✅', '⏩'].includes(reaction.emoji.name) && user.id === msg.author.id;
+                    };
 
-                data.setup = true;
+                    sentmsg.awaitReactions(filter, { max: 1, time: 24 * 60 * 60 * 1000, error: ['time'] }).then(collected => {
 
-                } else {
-                    channel.send("No problem chief, you can setup things later :smiley:");
-        }})})}}
+                        const reaction = collected.first();
+
+                        if (reaction.emoji.name === '✅') {
+                            let step = new Discord.MessageEmbed()
+                                .setColor(data.color)
+                                .setTitle('Step 1/?')
+                                .setDescription(':white_square_button: 1. Set the prefix. \n:white_square_button: 2. Set who has access to admins roles.');
+
+                            channel.send(step);
+
+                            data.setup = true;
+
+                        } else {
+                            channel.send("No problem chief, you can setup things later :smiley:");
+                        }
+                    })
+                })
+            }
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ process.on('unhandledRejection', (reason) => {
 });
 
 try {
-	var Discord = require("discord.js");
-} catch (error){
-	console.log(error.stack);
-	console.log("Please install dependencies with 'npm install --only=prod'"); // tell to install dependencies
-	process.exit();
+    var Discord = require("discord.js");
+} catch (error) {
+    console.log(error.stack);
+    console.log("Please install dependencies with 'npm install --only=prod'"); // tell to install dependencies
+    process.exit();
 }
 
 const config = require("./config.json");
-if(!Object.prototype.hasOwnProperty.call(config, 'BOT_TOKEN') || config.bot_token === 'YOUR BOT TOKEN') {
-	console.error("Please enter your bot token at ./src/data.json"); // tell to insert the bot token
-	process.exit()
+if (!Object.prototype.hasOwnProperty.call(config, 'BOT_TOKEN') || config.bot_token === 'YOUR BOT TOKEN') {
+    console.error("Please enter your bot token at ./src/data.json"); // tell to insert the bot token
+    process.exit()
 }
 
 const data = require("./data.json");
@@ -24,7 +24,7 @@ let adminCmds = require("./adminCmds.js"); // Admin commands
 
 const client = new Discord.Client();
 
-client.on("message", function(message) {
+client.on("message", function (message) {
     if (message.author.bot) return; // The message is from a bot
     if (!message.content.startsWith(data.prefix)) return; // The message don't concern me.
 
@@ -34,12 +34,9 @@ client.on("message", function(message) {
     if (args.length === 0) return; // There is no command
     const command = args.shift().toLowerCase();
 
-    if (Object.keys(adminCmds).includes(command)) { // Check if this is a command for admins
-        if (message.member.hasPermission('ADMINISTRATOR')) { // check if the user is an admin
-            return adminCmds[command].process(message);
-        } else {
-            return message.channel.reply("you do not have the right to use this command.");
-        }
+    // Check if this is a command for admins 1 if the user is an admin
+    if (Object.keys(adminCmds).includes(command) && (message.member.hasPermission('ADMINISTRATOR'))) {
+        return adminCmds[command].process(message);
     }
 });
 
