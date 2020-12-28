2c25dcf4b2641b17214af0769cbef1c8a667ef1c
tud <tudual.quinio@protonmail.com>
2020-12-28 13:38:39 +0100

    fix typo

--- src/commands/admin/roles.js
+++ src/commands/admin/roles.js
@@ -11,7 +11,7 @@ module.exports = {
             .setColor(process.env.color)
             .setTitle('Roles')
             .setDescription(`There are 2 differents roles, Admin and Moderator:
-            - Admin are people with the permissions administrator. They have access to dangerous command like \`${process.env.prefix}reset\`.
+            - Admin are people with the administrator permission. They have access to dangerous command like \`${process.env.prefix}reset\`.
             - Moderator are people who have access to more command than common mortal. You can add people to moderator through roles with \`${process.env.prefix}setmoderator\`.
             
             If you wonder who have access to wich command, you can use \`${process.env.prefix}commands <role>\` or \`${process.env.prefix}commands <someone>\`. You also see who has administrator permission with \`${process.env.prefix}admin\` and see who are moderators with \`${process.env.prefix}moderator\`.`);
