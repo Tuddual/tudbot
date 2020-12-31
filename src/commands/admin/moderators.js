44aebac503234b6a391ee011bb5c642d05fc0803
tud <tudual.quinio@protonmail.com>
2021-01-01 00:35:44 +0100

    rework !moderators

--- src/commands/admin/moderators.js
+++ src/commands/admin/moderators.js
@@ -1,37 +1,41 @@
 const { MessageEmbed } = require("discord.js");
 let data = require("../../data");
-const perm = require("../../permission");
 
 module.exports = {
-    description: 'Command to know who is a moderator',
+    description: 'Command to know everything about a moderator.',
+    long_description: `\`${data.prefix}admins\` is a command to know who are moderators.
+    This command also mention how to become a moderator.`,
     use: `moderators`,
-    process: (msg) => {
+    process: async (msg) => {
 
-        msg.guild.members.fetch()
-        .then( members => {
-
-            let moderators = []
-            members.each(GuildMember => {
-                if (perm.isMod(GuildMember) && !GuildMember.user.bot) {
-                    moderators.push(GuildMember.user.id);
+        for (const askrole of data.moderator) {
+            await msg.guild.roles.fetch(askrole)
+            .then( role => {
+                if (role === null) {
+                    data.moderator = data.moderator.filter(item => item !== askrole);
+                    data.save();
                 }
-            })
-        
-            let description = `Moderators are people who have access to more command than common mortal.
-            If you want to allow someone to have access to moderator commands, s·he just need to have a moderator role. To manage moderators roles use \`${data.prefix}setmoderators\`. \n
-            There ${moderators.length > 1 ? 'are' : 'is'} actually ${moderators.length} moderator${moderators.length > 1 ? 's' : ''} : \n`;
-            for (const item of moderators) {
-                description += ` - <@${item}>\n`;
-            }
-    
-            const embed = new MessageEmbed()
-                .setColor(data.color)
-                .setTitle('Moderators')
-                .setDescription(description);
-    
-            msg.channel.send(embed);
-        }).catch(() => {
-            console.error("You need enable the option 'Server Members Intent' at 'https://discord.com/developers/applications'");
+            }).catch(error => console.error(error));
+        }
+
+        let description = `Moderators are people who have access to more command than regular people.
+        If you want to give someone access to moderator commands, they just need to have a moderator role.\n    
+        There ${data.moderator.length > 1 ? 'are' : 'is'} ${data.moderator.length} role${data.moderator.length > 1 ? 's' : ''} attributed as moderator : \n`;
+        for (const item of data.moderator) {
+            description += ` - <@&${item}>\n`;
+        }
+
+        description += `\n If you want to add a moderator role, use \`${data.prefix}addmoderator <role>\`
+         If you want to remove a moderator role, use \`${data.prefix}delmoderator <role>\``;
+
+        const embed = new MessageEmbed()
+            .setColor(data.color)
+            .setTitle('Moderators')
+            .setDescription(description);
+
+        msg.channel.send(embed)
+        .catch((error) => {
+            console.error(error);
             msg.react('😞').catch(error => console.error(error));
         });
     }
